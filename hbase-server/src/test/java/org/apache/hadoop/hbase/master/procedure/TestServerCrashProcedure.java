539efe02f02bb56f1b6b6c4b780bfc6c12794172
Gaurav Kanade <gaurav.kanade@gmail.com>
2020-09-09 16:54:46 -0700

    deep debug

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/procedure/TestServerCrashProcedure.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/procedure/TestServerCrashProcedure.java
@@ -171,9 +171,9 @@ public class TestServerCrashProcedure {
     LOG.info("Submitted count" + this.util.getHBaseCluster().getMaster().getMasterMetrics().getServerCrashProcMetrics().getSubmittedCounter().getCount());
     LOG.info("Histo count"+ this.util.getHBaseCluster().getMaster().getMasterMetrics().getServerCrashProcMetrics().getTimeHisto().getCount());
     MetricsMasterSource masterSource = this.util.getHBaseCluster().getMaster().getMasterMetrics().getMetricsSource();
-    LOG.info("Histo min " + masterSource.getServerCrashMetrics().getTimeHisto().snapshot().getMin());
+    //LOG.info("Histo min " + masterSource.getServerCrashMetrics().getTimeHisto().snapshot().getMin());
 
-    LOG.info("Histo max " + masterSource.getServerCrashMetrics().getTimeHisto().snapshot().getMax());
+    //LOG.info("Histo max " + masterSource.getServerCrashMetrics().getTimeHisto().snapshot().getMax());
     /*metricsHelper.assertCounter(MetricsMasterSource.SERVER_CRASH_METRIC_PREFIX+"SubmittedCount",
             4, masterSource);*/
     metricsHelper.assertGauge(MetricsMasterSource.SERVER_CRASH_METRIC_PREFIX+"Time_min",
